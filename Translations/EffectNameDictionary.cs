1a4733cacd3840dfb78c8073a04f5b54af10128d
Kamelowy <62029781+v-kamelowy@users.noreply.github.com>
2022-09-05 07:21:35 +0200

    Przecinki + komentarz

--- Translations/EffectNameDictionary.cs
+++ Translations/EffectNameDictionary.cs
@@ -10,29 +10,26 @@ namespace CalamityPL
                 Szybki tutorial tłumaczenia dla nietechnicznych:
                         { "ID", "Tłumaczenie" }, 
             */
-            /*
-            { "AlcoholPoisoning", "Zatrucie alkoholowe" },
-            {"BloodyMaryBuff", "Krwawa Mary"}
-            {"CaribbeanRumBuff", "Rum Karaibski"}
-            {"CinnamonRollBuff", "Koktajl z cynamonem"}
-            {"EverclearBuff", "Everclear"}
-            {"EvergreenGinBuff", "Gin Evergreen"}
-            {"FabsolVodkaBuff", "Fabsolówka"}
-            {"FireballBuff", "Fireball"}
-            {"MargaritaBuff", "Margarita"}
-            {"MoonshineBuff", "Bimber"}
-            {"MoscowMuleBuff", "Muł Moskiewski"}
-            {"RedWineBuff", "Wino Czerwone"}
-            {"RumBuff", "Rum"}
-            {"ScrewdriverBuff", "Screwdriver"}
-            {"StarBeamRyeBuff", "Gwiezdne Whisky"}
-            {"TequilaBuff", "Tequila"}
-            {"Trippy", "Halucynacje"}
-            {"VodkaBuff", "Wódka"}
-            {"WhiskeyBuff", "Whisky"}
-            {"WhiteWineBuff", "Wino Białe"}
-            
-            */
+            {"AlcoholPoisoning", "Zatrucie alkoholowe"},
+            {"BloodyMaryBuff", "Krwawa Mary"},
+            {"CaribbeanRumBuff", "Rum Karaibski"},
+            {"CinnamonRollBuff", "Koktajl z cynamonem"},
+            {"EverclearBuff", "Everclear"},
+            {"EvergreenGinBuff", "Gin Evergreen"},
+            {"FabsolVodkaBuff", "Fabsolówka"},
+            {"FireballBuff", "Fireball"},
+            {"MargaritaBuff", "Margarita"},
+            {"MoonshineBuff", "Bimber"},
+            {"MoscowMuleBuff", "Muł Moskiewski"},
+            {"RedWineBuff", "Wino Czerwone"},
+            {"RumBuff", "Rum"},
+            {"ScrewdriverBuff", "Screwdriver"},
+            {"StarBeamRyeBuff", "Gwiezdne Whisky"},
+            {"TequilaBuff", "Tequila"},
+            {"Trippy", "Halucynacje"},
+            {"VodkaBuff", "Wódka"},
+            {"WhiskeyBuff", "Whisky"},
+            {"WhiteWineBuff", "Wino Białe"},
         };
     }
 }
